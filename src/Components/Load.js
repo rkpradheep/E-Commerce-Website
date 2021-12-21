b37c1220005b8d482a494af8645a7ff8cded8fe5
rkpradheep <pradheep.rk.it@gmail.com>
2021-12-21 23:11:38 +0530

    Changes in Load icon

--- src/Components/Load.js
+++ src/Components/Load.js
@@ -8,9 +8,9 @@ return (
 	<div style={{margin:"0",top:"40%",transform:"translateY(-40%)",left:"50%",transform:"translateX(-50%)",position:"absolute"}}>
 	<ReactLoading
 		type="spokes"
-		color="blue"
-		height={150}
-		width={150}
+		color="lightblack"
+		height={40}
+		width={40}
 	/>
 	</div>
 	</div>
